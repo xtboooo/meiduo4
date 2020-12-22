755b758d084ffb213f7e56f413e449cfec83ec14
xtbo97 <xtbo9719@gmail.com>
2020-12-22 20:09:52 +0800

    用户注册-短信验证码发送

--- meiduo_mall/meiduo_mall/apps/verifications/urls.py
+++ meiduo_mall/meiduo_mall/apps/verifications/urls.py
@@ -7,5 +7,6 @@ from verifications import views
 
 urlpatterns = {
     re_path(r'^image_codes/(?P<uuid>[\w-]+)/$', views.ImageCodeView.as_view()),
-    
+    re_path(r'^sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),
+
 }
--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponse
+import random
 
+from django.http import HttpResponse, JsonResponse
 from django.views import View
-
 from django_redis import get_redis_connection
 from meiduo_mall.libs.captcha.captcha import captcha
+
+import logging
+
+from meiduo_mall.libs.yuntongxun.ccp_sms import CCP
+
+logger = logging.getLogger('django')
 
 
 # GET /image_codes/(?P<uuid>[\w-]+)/
@@ -19,3 +25,51 @@ class ImageCodeView(View):
 
         # 3.返回图片验证码
         return HttpResponse(image, content_type='image/jpg')
+
+
+# GET /sms_codes/(?P<mobile>1[3-9]\d{9})/
+class SMSCodeView(View):
+    def get(self, request, mobile):
+        """ 获取短信验证码 """
+        # 1.获取参数并校验
+        image_code = request.GET.get('image_code')
+        uuid = request.GET.get('image_code_id')
+
+        # 验证参数完整性
+        if not all([image_code, uuid]):
+            return JsonResponse({'code': 400,
+                                 'message': '缺少必传参数!'})
+
+        # 2.对比图片验证码
+        # 获取redis中的图形验证码
+        redis_conn = get_redis_connection('verify_code')
+
+        redis_image_code = redis_conn.get('img_%s' % uuid)
+
+        if redis_image_code is None:
+            return JsonResponse({'code': 400,
+                                 'message': '图形验证码失效!'})
+
+        # 删除redis中的文本验证码
+        try:
+            redis_conn.delete('img_%s' % uuid)
+        except Exception as e:
+            logger.error(e)
+
+        if image_code.lower() != redis_image_code.lower():
+            return JsonResponse({'code': 400,
+                                 'message': '输入图形验证码有误!'})
+
+        # 3.生成保存并发送短信验证码
+        sms_code = '%06d' % random.randint(0, 999999)
+        logger.info('短信验证码为:%s' % sms_code)
+
+        # 保存短信验证码
+        redis_conn.set('sms_%s' % mobile, sms_code, 300)
+
+        # 发送短信验证码
+        CCP().send_template_sms(mobile, [sms_code, 5], 1)
+
+        # 返回响应
+        return JsonResponse({'code': 0,
+                             'message': '发送短信成功!'})
