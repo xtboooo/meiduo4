59fff933251378444675f9475f86c3717e154aa5
xtbo97 <xtbo9719@gmail.com>
2020-12-22 19:27:05 +0800

    用户注册-手机号是否重复注册

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -17,3 +17,19 @@ class UsernameCountView(View):
         return JsonResponse({'code': 0,
                              'message': 'OK',
                              'count': count, })
+
+
+# GET /mobiles/(?P<mobile>1[3-9]\d{9})/count/
+class MobileCountView(View):
+    def get(self, request, mobile):
+        """ 判断注册手机号是否重复 """
+        # 1.查询数据库判断mobile是否存在
+        try:
+            count = User.objects.filter(mobile=mobile).count()
+        except Exception as e:
+            return JsonResponse({'code': 400,
+                                 'message': '操作数据库失败!'})
+        # 2.返回响应
+        return JsonResponse({'code': 0,
+                             'message': 'OK',
+                             'count': count, })
